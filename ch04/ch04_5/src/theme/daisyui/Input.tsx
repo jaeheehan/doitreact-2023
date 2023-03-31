a49189698f4534b529a95e1c2d028617d1c775be
jaehee <jaehee23@naver.com>
2023-03-31 10:58:36 +0900

    react hook - ch04_5 ForwardRefTest

--- ch04/ch04_5/src/theme/daisyui/Input.tsx
+++ ch04/ch04_5/src/theme/daisyui/Input.tsx
@@ -1,10 +1,12 @@
 import type { FC, DetailedHTMLProps, InputHTMLAttributes} from "react";
+import {forwardRef} from "react";
 
 export type ReactInputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-export type inputProps = ReactInputProps & {}
+export type InputProps = ReactInputProps & {}
 
-export const Input: FC<inputProps> = ({className: _className, ...inputProps}) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+    const {className: _className, ...inputProps} = props
     const className = ['input', _className].join(' ')
-    return <input {...inputProps} className={className}/>
-}
+    return <input ref={ref} {...inputProps} className={className}/>
+})
