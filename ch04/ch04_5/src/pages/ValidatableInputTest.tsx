7c4e89a07b5f1aaa8af1c378c2584be430d26161
jaehee <jaehee23@naver.com>
2023-04-05 17:25:39 +0900

    react hook - ch04_5 validatableInput 작업

--- ch04/ch04_5/src/pages/ValidatableInputTest.tsx
+++ ch04/ch04_5/src/pages/ValidatableInputTest.tsx
@@ -1,8 +1,23 @@
-export default function CopyMe() {
-    return (
+import {useRef, useCallback} from "react";
+import {Title} from "../components";
+import type {ValidatableInputMethods} from "../theme/daisyui/ValidatableInput";
+import {ValidatableInput} from "../theme/daisyui/ValidatableInput";
+
+export default function ValidatableInputTest() {
+    const methodsRef = useRef<ValidatableInputMethods>(null)
+
+    const validateEmail = useCallback(()=> {
+        if(methodsRef.current) {
+            const [valid, valueOrErrorMessage] = methodsRef.current.validate()
+            if (valid) alert(`${valueOrErrorMessage}는 유효한 이메일 주소입니다.`)
+            else alert(valueOrErrorMessage)
+        }
+    }, [])
+
+    return  (
         <section className="mt-4">
-            <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-            <div className="mt-4"></div>
+            <Title>ValidatableInputTest</Title>
+            <div className="flex justify-center mt-4"></div>
         </section>
     )
 }
