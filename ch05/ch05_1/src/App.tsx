b418a137702f603585e9f894fc7b91d11ce6eb3e
jaehee <jaehee23@naver.com>
2023-04-06 16:09:10 +0900

    react hook - ch05_1 redux

--- ch05/ch05_1/src/App.tsx
+++ ch05/ch05_1/src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReduxClock from './pages/ReduxClock'
+import UseReducerClock from './pages/UseReducerClock'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <main className="p-8">
+        <UseReducerClock/>
+        <ReduxClock/>
+      </main>
+  )
 }
-
-export default App;
