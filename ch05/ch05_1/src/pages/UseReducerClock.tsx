ecefe465dbe7970b4885dcea446506e97192da5d
jaehee <jaehee23@naver.com>
2023-04-10 16:11:55 +0900

    react hook - ch05_1 use Reducer Clock

--- ch05/ch05_1/src/pages/UseReducerClock.tsx
+++ ch05/ch05_1/src/pages/UseReducerClock.tsx
@@ -1,8 +1,31 @@
-export default function CopyMe() {
+import { useReducer } from "react";
+import type {AppState} from "../store";
+import type {SetTodayAction} from "../store/actions";
+import {Div, Title, Subtitle } from "../components";
+import {useInterval } from "../hooks";
+
+export default function UseReducerClock(){
+    const [{today}, dispatch] = useReducer(
+        (state: AppState, action: SetTodayAction) => {
+            switch (action.type) {
+                case 'setToday':
+                    return {...state, today: new Date()}
+            }
+        },
+        {
+            today: new Date()
+        }
+    )
+
+    useInterval(()=> {
+        dispatch({type: 'setToday', today: new Date()})
+    })
+
     return (
-        <section className="mt-4">
-            <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-            <div className="mt-4"></div>
-        </section>
+        <Div className="flex flex-col items-center justify-center mt-16">
+            <Title className="text-5xl">UseReducerClock</Title>
+            <Title className="mt-4 text-3xl">{today.toLocaleTimeString()}</Title>
+            <Subtitle className="mt-4 text-2xl">{today.toLocaleDateString()}</Subtitle>
+        </Div>
     )
 }
--- ch05/ch05_1/src/store/rootReducer.ts
+++ ch05/ch05_1/src/store/rootReducer.ts
@@ -1,7 +1,16 @@
-import type { Action } from 'redux'
+import type { Actions } from './actions'
 import type { AppState} from "./AppState";
 
 const initialAppState = {
     today: new Date()
 }
-export const rootReducer = (state: AppState = initialAppState, action: Action) => state
+export const rootReducer = (state: AppState = initialAppState, action: Actions) => {
+
+    switch (action.type) {
+        case 'setToday': {
+            return {...state, today: action.today}
+        }
+    }
+    return state
+
+}
