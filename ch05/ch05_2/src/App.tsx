7a008fb383fb4f2d3a06bdf9ba8a4e2d4aae5908
jaehee <jaehee23@naver.com>
2023-04-11 16:51:27 +0900

    react hook - ch05_2 init redux

--- ch05/ch05_2/src/App.tsx
+++ ch05/ch05_2/src/App.tsx
@@ -1,19 +1,11 @@
-import {Provider as ReduxProvider } from 'react-redux'
-import {useStore} from "./store";
-
-
-import ReduxClock from './pages/ReduxClock'
-import UseReducerClock from './pages/UseReducerClock'
+import { Provider as ReduxProvider } from "react-redux";
+import { useStore } from "./store";
 
 export default function App() {
     const store = useStore()
-  return (
-      <ReduxProvider store={store}>
-        <main className="p-8">
-          <UseReducerClock/>
-          <ReduxClock/>
-        </main>
-      </ReduxProvider>
-
-  )
+    return (
+        <ReduxProvider store={store}>
+            <div/>
+        </ReduxProvider>
+    )
 }
